8cee01573ef79c76ebfba498b142ebd52f41442f
pp811010 <pp811010@gmail.com>
2026-06-17 11:11:58 +0700

    fix error lenis2

--- components/providers/LenisProvider.tsx
+++ components/providers/LenisProvider.tsx
@@ -12,8 +12,7 @@ export default function LenisProvider({ children }: { children: ReactNode }) {
   useEffect(() => {
     const lenis = new Lenis({
       duration: 1.2,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),   
-      mouseMultiplier: 1,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       touchMultiplier: 2,
     });
 
